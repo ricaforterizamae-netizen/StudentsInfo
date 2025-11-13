140110f45803a1af2dc9ca21aee064e12f4fae27
ricaforterizamae-netizen <ricaforterizamae@gmail.com>
2025-11-14 07:42:12 +0800

    Add files via upload

---
+++ NetBeansProjects/rebalde/src/rebalde/Rebalde.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rebalde;
+
+/**
+ *
+ * @author rizarebalde
+ */
+public class Rebalde {
+    public static void main(String[] args) {
+        StudentInfos stud = new StudentInfos();
+        stud.setVisible(true);
+    }
+    
+}
